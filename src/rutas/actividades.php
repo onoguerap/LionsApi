b98124fb45b30599c9e9fc127e3ae25d4b27c2a1
pedro <pedrotaylor.45@gmail.com>
2020-06-12 00:15:30 -0600

    Este push agrega la consulta de actividades de la base de datos (correccion)

--- src/rutas/actividades.php
+++ src/rutas/actividades.php
@@ -6,7 +6,6 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 // GET Obtener los miembros por filtro o la totalidad
 $app->get('/api/actividades', function(Request $request, Response $response){
-    echo 'entrando a la vara mae';
     $message = '';
     $activities = array();
 
@@ -32,84 +31,4 @@ $app->get('/api/actividades', function(Request $request, Response $response){
     } catch (PDOException $e) {
         echo '{"error" : {"text":'.$e.getMessage().'}';
     }
-}
-/*
-// GET Obtener la información del miembro
-$app->get('/api/miembro', function(Request $request, Response $response){
-		$id_member = $request->getParam('id_member');
-    $message = '';
-    $result = 0;
-		$members = array();
-
-		if (strlen($id_member) == 0) {
-			$out['ok'] = 1;
-			$out['result'] = 0;
-			$out['message'] = "No se enviaron los datos necesarios!";
-			$out['data'] = $members;
-			echo json_encode($out, JSON_UNESCAPED_UNICODE);
-			die();
-		}
-		
-		$sql = "SELECT M.*, C.name_club, T.description type_member, R.description rol_member
-		FROM tb_members M
-		INNER JOIN tb_clubs C ON M.club_code = C.club_code
-		INNER JOIN tb_type_members T ON M.id_type_member = T.id_type_member
-    INNER JOIN tb_rol R ON M.id_rol_member = R.id_rol_member
-		WHERE M.status = 1
-		AND M.id_member = $id_member
-		LIMIT 1;";	
-    
-    try {
-        $db = new db();
-        $db = $db->dbConnection();
-        $resultado = $db->query($sql);
-        if ($resultado->rowCount() > 0) {
-            $members = $resultado->fetchAll(PDO::FETCH_OBJ);
-            $result  = 1;
-        } else {
-            $result = 0;
-            $message = "No se encontraron miembros con ese ID!";
-        }
-        $out['ok'] = 1;
-        $out['result'] = $result;
-        $out['message'] = $message;
-				$out['data'] = $members;
-        echo json_encode($out, JSON_UNESCAPED_UNICODE);
-    } catch (PDOException $e) {
-        echo '{"error" : {"text":'.$e.getMessage().'}';
-    }
 });
-
-// GET Obtener los miembros por filtro o la totalidad
-$app->get('/api/birthdays', function(Request $request, Response $response){
-		
-    $message = '';
-    $result = 0;
-		$members = array();
-		
-		$sql = "SELECT CONCAT_WS(' ', name, last_name) fullname, DATE_FORMAT(birthday, '%d/%m') min_date, IF(DATE_FORMAT(birthday, '%d') = DATE_FORMAT(SYSDATE(), '%d'), 1, 0) is_today
-		FROM tb_members
-		WHERE DATE_FORMAT(birthday, '%m') = DATE_FORMAT(SYSDATE(), '%m')
-		AND DATE_FORMAT(birthday, '%d') >= DATE_FORMAT(SYSDATE(), '%d')
-		ORDER BY DATE_FORMAT(birthday, '%d/%m') ASC;";	
-    
-    try {
-        $db = new db();
-        $db = $db->dbConnection();
-        $resultado = $db->query($sql);
-        if ($resultado->rowCount() > 0) {
-            $members = $resultado->fetchAll(PDO::FETCH_OBJ);
-            $result  = 1;
-        } else {
-            $result = 0;
-            $message = "No se encontraron cumpleañeros!";
-        }
-        $out['ok'] = 1;
-        $out['result'] = $result;
-        $out['message'] = $message;
-				$out['data'] = $members;
-        echo json_encode($out, JSON_UNESCAPED_UNICODE);
-    } catch (PDOException $e) {
-        echo '{"error" : {"text":'.$e.getMessage().'}';
-    }
-}*/);
